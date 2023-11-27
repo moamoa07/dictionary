fed096d65d19f490c85f64209a928c5803cc2cdd
Moa Hedendahl <moahedendahl@gmail.com>
2023-11-27 11:27:57 +0100

    set up tests

---
+++ src/test/Searchbar.test.tsx
@@ -0,0 +1,78 @@
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import Searchbar from '../components/Searchbar';
+
+test('should see if searchfield exists', () => {
+  render(<Searchbar words={[]} />);
+});
+
+test('should accept a user typing', async () => {
+  render(<Searchbar words={[]} />);
+  const user = userEvent.setup();
+  const input = screen.getByRole('textbox');
+
+  await user.type(input, 'hej');
+
+  expect(input).toHaveValue('hej');
+});
+
+test('should display word after submission via click', async () => {
+  render(<App />);
+  const user = userEvent.setup();
+
+  const input = screen.getByRole('textbox');
+  await user.type(input, 'cat');
+  const searchBtn = screen.getByText('Search');
+  await user.click(searchBtn);
+
+  const list = await screen.findByTestId('list-word-phonetics');
+  expect(within(list).getByText('cat')).toBeInTheDocument();
+  expect(within(list).getByText('Phonetic: /kat/')).toBeInTheDocument();
+  expect(within(list).getByText('Part of Speech: noun'));
+});
+
+test('should see if an error displays if search with empty field', async () => {
+  render(<Searchbar words={[]} />);
+  const user = userEvent.setup();
+
+  const searchBtn = screen.getByText('Search');
+  await user.click(searchBtn);
+
+  const errorElement = await screen.findByTestId('error');
+  expect(
+    within(errorElement).getByText('Please enter a word to search.')
+  ).toBeInTheDocument();
+});
+
+test('should see if an error displays if search with word that does not exist', async () => {
+  render(<Searchbar words={[]} />);
+  const user = userEvent.setup();
+
+  const input = screen.getByRole('textbox');
+  await user.type(input, 'hej');
+  const searchBtn = screen.getByText('Search');
+  await user.click(searchBtn);
+
+  const list = await screen.findByTestId('error');
+  expect(
+    within(list).getByText('Word does not exist, try something else.')
+  ).toBeInTheDocument();
+});
+
+test('should see if there is an audio element', async () => {
+  render(<Searchbar words={[]} />);
+  const user = userEvent.setup();
+
+  const input = screen.getByRole('textbox');
+  await user.type(input, 'cat');
+  const searchBtn = screen.getByText('Search');
+  await user.click(searchBtn);
+
+  const list = await screen.findByTestId('list-word-phonetics');
+  const audioElements = within(list).getAllByTestId('audio');
+  expect(audioElements[0]).toHaveAttribute(
+    'src',
+    'https://api.dictionaryapi.dev/media/pronunciations/en/cat-uk.mp3'
+  );
+});
